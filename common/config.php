4337f2163d5d816de1dd9e10d80ef53f8d888115
[guille9492] <[guillermogonzalezmiranda@gmail.com]>
2024-03-13 10:40:44 +0100

    new file:   admin/actions/edit_foto.act.php 	new file:   admin/actions/listado.act.php 	new file:   admin/actions/login.act.php 	new file:   admin/actions/new.act.php 	new file:   admin/actions/new_foto.act.php 	new file:   admin/delete.php 	new file:   admin/home.php 	new file:   admin/includes/edit_foto.inc.php 	new file:   admin/includes/listado.inc.php 	new file:   admin/includes/listado_autores.inc.php 	new file:   admin/includes/login.inc.php 	new file:   admin/includes/menu.php 	new file:   admin/includes/new.inc.php 	new file:   admin/includes/new_foto.inc.php 	new file:   admin/index.php 	new file:   assets 	new file:   common/config.php 	new file:   common/mysql.php 	new file:   common/utils.php 	new file:   images/digital_camera_photo-1080x675.jpg 	new file:   images/foto.jpg 	new file:   images/instagram-image-size.jpg 	new file:   images/sunset_ocean_tree.jpg 	new file:   index.php 	deleted:    prueba.txt 	deleted:    readme

---
+++ common/config.php
@@ -0,0 +1,15 @@
+<?php
+
+    $config = array(
+
+        'database' => array(
+            'host' => 'localhost',
+            'username' => 'phpmyadmin',
+            'password' => 'phpmyadmin',
+            'database' => 'GALLERY2',
+            'encoding' => 'utf8',
+        ),
+    );
+
+
+?>
